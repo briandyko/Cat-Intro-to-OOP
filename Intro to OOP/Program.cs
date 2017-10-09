26b128b62153959855e33317645e26d4cb738600
briandyko <briandyko@gmail.com>
2017-10-09 14:10:51 -0400

    second dog sample

--- Intro to OOP/Program.cs
+++ Intro to OOP/Program.cs
@@ -10,23 +10,60 @@ namespace Intro_to_OOP
     {
         static void Main(string[] args)
         {
-            Cat mittens = new Cat();
+            //Cat mittens = new Cat();
 
-            mittens.Name = "Mittens";
+            //mittens.Name = "Mittens";
 
-            Cat secondCat = new Cat("Oz", 3, "orange and white");
+            //Cat secondCat = new Cat("Oz", 3, "orange and white");
 
-            Console.WriteLine(mittens.Name);
-            Console.WriteLine(secondCat.Name);
+            //Console.WriteLine(mittens.Name);
+            //Console.WriteLine(secondCat.Name);
 
-            mittens.Eat();
+            //mittens.Eat();
 
-            mittens.WorkOut();
+            //mittens.WorkOut();
 
+            //Dog firstDoge = new Dog();
 
+            //firstDoge.Weight = 60.00;
+            //firstDoge.Height = 3.2f;
+            //firstDoge.FurLength = "flowing mane";
+            //firstDoge.RunningSpeed = "will fetch.....eventually.";
 
+            //firstDoge.Groom();
+            //Console.WriteLine(firstDoge.FurLength);
 
+            //firstDoge.Run();
+            //Console.WriteLine("The dog now weighs " + firstDoge.Weight);
+            //Console.WriteLine(firstDoge.RunningSpeed);
 
+            //firstDoge.Bark();
+
+            Dog secondDoge = new Dog("fluffy", 4.04f);
+
+            Console.WriteLine(secondDoge.FurLength);
+
+            secondDoge.Groom();
+
+            Console.WriteLine(secondDoge.FurLength);
+
+            secondDoge.RunningSpeed = "faster than lightning.";
+
+            Console.WriteLine(secondDoge.RunningSpeed);
+
+            secondDoge.Run();
+
+            Console.WriteLine(secondDoge.RunningSpeed);
+
+            secondDoge.Weight = 78.9;
+
+            Console.WriteLine(secondDoge.Weight);
+
+            secondDoge.Potty();
+
+            Console.WriteLine(secondDoge.Weight);
+
+            secondDoge.Bark();
         }
     }
 }
